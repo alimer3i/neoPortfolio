e36522d6ac9b968c3f4a39832c452db7dbd3820b
Ali Merhi <ali.merhi@montymobile.com>
2022-02-05 11:22:01 +0200

    Initial Commit

---
+++ NeoPortfolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoPortfolio
+//
+//  Created by Ali Merhie on 2/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
